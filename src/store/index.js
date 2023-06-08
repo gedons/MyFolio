dbd123d78121b76f0e7c995c3fc767301ccf6e96
gedons <gedoniani@gmail.com>
2023-06-08 15:49:11 +0100

    First Commit

---
+++ src/store/index.js
@@ -0,0 +1,18 @@
+import { createStore } from "vuex";
+
+const store = createStore({
+    state:{
+        user:{
+            data:{
+
+            },
+            token: null
+        }
+    },
+    getters:{},
+    actions:{},
+    mutations:{},
+    modules:{}
+});
+
+export default store;

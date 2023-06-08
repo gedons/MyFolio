47de3eba50ac8aabe8cd26e6fcfca9a406e4f0d6
gedons <gedoniani@gmail.com>
2023-06-08 19:12:29 +0100

    created admin dashboard and pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,10 @@
 import { createRouter, createWebHistory } from "vue-router";
+import store from "../store";
 import Index from "../views/Index.vue";
+import Admin from "../views/Admin/Index.vue";
+import Project from "../views/Admin/Project.vue";
+import Ablog from "../views/Admin/Blog.vue";
+import Message from "../views/Admin/Message.vue";
 import Login from "../views/Login.vue";
 import Blog from "../views/Blog.vue";
 
@@ -9,6 +14,16 @@ const routes = [
       name: 'Index',
       component: Index
     },
+
+    {
+      path: "/admin",
+      name: "Admin",
+      component: Admin,
+      meta: { requiresAuth: true },
+    },
+    { path: "/admin/project", name: "Project", component: Project, meta: { requiresAuth: true }, },
+    { path: "/admin/blog", name: "Ablog", component: Ablog, meta: { requiresAuth: true }, },
+    { path: "/admin/message", name: "Message", component: Message, meta: { requiresAuth: true }, },
 
     {
       path: "/login",
@@ -27,6 +42,16 @@ const routes = [
 const router = createRouter({
     history: createWebHistory(),
     routes,
+  });
+
+  router.beforeEach((to, from, next) => {
+    if (to.meta.requiresAuth && !store.state.user.token) {
+      next({ name: "Login" });
+    } else if (store.state.user.token && to.meta.isGuest) {
+      next({ name: "Admin" });
+    } else {
+      next();
+    }
   });
 
 export default router;
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -4,7 +4,7 @@
     <div class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
       <div class="sm:mx-auto sm:w-full sm:max-w-sm">
         <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company" />
-        <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to your account</h2>
+        <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Welcome Gedoni</h2>
       </div>
   
       <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
@@ -34,9 +34,9 @@
         </form>
   
         <p class="mt-10 text-center text-sm text-gray-500">
-          Not a member?
+          Exclusive
           {{ ' ' }}
-          <a href="#" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Start a 14 day free trial</a>
+          <a href="#" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Access</a>
         </p>
       </div>
     </div>

1912c7ca4859a37a6d973b8afe4b34013c34b3e6
Nerdy Gedoni <gedoniani@gmail.com>
2024-01-24 14:41:07 +0100

    endpoinr api updated

--- src/api.js
+++ src/api.js
@@ -1,3 +1,5 @@
-const api = 'http://localhost:5000/api/v1';
+//const api = 'http://localhost:5000/api/v1';
+const api = 'https://folio-gk6i.onrender.com/api/v1';
+
 
 export default api;
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api/v1', 
+  //baseURL: 'http://localhost:5000/api/v1',
+  baseURL: 'https://folio-gk6i.onrender.com/api/v1', 
+  
 });
 
 export default api;
